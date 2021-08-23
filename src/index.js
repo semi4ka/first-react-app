d75201e25dc2edf05bdb971d920a4f46f736ab97
Sergii Sukhoi <mayulka@Sergiis-MacBook-Pro.local>
2021-08-23 22:13:52 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+const elem = <h1>Hello World, my name is Serhii Bevz!</h1>;
+
+ReactDom.render(elem, document.getElementById("root"));
